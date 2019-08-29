65f18d63b42126e24c68a99eabc8bc8191400c40
lvxinlong <1511325099@qq.com>
2019-08-29 13:11:54 +0800

    # [lvxinlong]-change enter.py and add settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+"""
+    :author: long
+    :DATE: 2019/8/29 13:09
+    :copyright: © 2019 long <lvxinlongyouxiang@163.com>
+    :license: None, see LICENSE for more details.
+"""
+name = 'lv'
+
+
